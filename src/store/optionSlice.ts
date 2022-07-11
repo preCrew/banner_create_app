04304d148f0118b338912e0bbb5727873e4312e0
송민형 <>
2022-07-11 22:49:58 +0900

    feat: redux store 추가완료

---
+++ src/store/optionSlice.ts
@@ -0,0 +1,69 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type TTextShadowType =  "x" | "y" | "blur";
+interface TOptionState {
+    backgroundColor: string;
+    foregroundColor: string;
+    borderColor: string;
+    borderWidth: number;
+    fontSize: number;
+    textShadow: {
+        x?: number;
+        y?: number;
+        blur?: number;
+    };
+    heightRatio: number;
+};
+const initialContentsState: TOptionState = {
+    backgroundColor: '',
+    foregroundColor: '',
+    borderColor: '',
+    borderWidth: 0,
+    fontSize: 0,
+    textShadow: {
+        x: 0,
+        y: 0,
+        blur: 0
+    },
+    heightRatio: 0
+};
+
+const contentsSlice = createSlice({
+    name: 'optionSlice',
+    initialState: initialContentsState,
+    reducers: {
+        changeBackgroundColor: (state: TOptionState, action: PayloadAction<string>) => {
+            state.backgroundColor = action.payload;
+        },
+        changeForegroundColor: (state: TOptionState, action: PayloadAction<string>) => {
+            state.foregroundColor = action.payload;
+        },
+        changeBorderColor: (state: TOptionState, action: PayloadAction<string>) => {
+            state.borderColor = action.payload;
+        },
+        changeBorderWidth: (state: TOptionState, action: PayloadAction<number>) => {
+            state.borderWidth = action.payload;
+        },
+        changeFontSize: (state: TOptionState, action: PayloadAction<number>) => {
+            state.fontSize = action.payload;
+        },
+        changeTextShadow : (state: TOptionState, action: PayloadAction<{type: TTextShadowType, value: number}>) => {
+            state.textShadow[action.payload.type] = action.payload.value;
+        },
+        changeHeightRatio: (state: TOptionState, action: PayloadAction<number>) => {
+            state.heightRatio = action.payload;
+        },
+    },
+});
+
+
+export const { 
+    changeBackgroundColor,  
+    changeForegroundColor,
+    changeBorderColor,
+    changeBorderWidth,
+    changeFontSize,
+    changeTextShadow,
+    changeHeightRatio
+} = contentsSlice.actions;
+export default contentsSlice.reducer;
