63a56954c24826639c863883fca24f84f6e3cc97
송민형 <>
2022-07-12 18:01:42 +0900

    change: mockup data to real component
    목업으로 만들었던 데이터들
    실제 컴포넌트 생성한걸로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import Content from './components/Content';
+import Header from './components/Header';
 
 const App = () => {
 
   return (
     <AppBlock>
-      <TempHeader/>
-      <Content>
-        <TempBanner/>
-        <TempControls/>
-      </Content>
+      <Header/>
+      <Content/>
     </AppBlock>
   );
 };
@@ -19,41 +18,4 @@ export default App;
 const AppBlock = styled.div`
   width: 100%;
   height: 100%;
-`;
-
-// TODO: Header 컴포넌트로 교체
-const TempHeader = styled.div`
-  width:100%;
-  padding: 16px;
-  height: 80px;
-  background: grey;
-`;
-
-const Content = styled.div`
-  width: 100%;
-
-  display: flex;
-  justify-content: center;
-
-  @media (max-width: 880px){
-    flex-direction: column;
-  }
-`;
-
-// TODO: Banner 컴포넌트로 교체
-const TempBanner = styled.div`
-  height: 30vw;
-  margin: 16px;
-  background: #187818;
-  flex-grow: 1;
-  position: sticky;
-  top: 0;
-`;
-
-// TODO: Controls 컴포넌트로 교체
-const TempControls = styled.div`
-  min-width: 310px;
-  height: 980px;
-  margin: 16px;
-  background: #182378;
 `;
