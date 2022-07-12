3752f934f1fdb20646f61113657b5cedf7b11993
송민형 <>
2022-07-12 17:49:08 +0900

    add: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import Header from './index'
+
+interface indexProps {
+    
+}
+
+const index = () => {
+    return (
+        <Header>
+        </Header>
+    );
+}
+
+export default index;
