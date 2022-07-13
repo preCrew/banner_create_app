a2dadf348c923b2ff9d54a4304ff29b565d1b130
송민형 <>
2022-07-13 19:27:21 +0900

    test: color picker use example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Content from './components/Content';
+import ColorPicker from './components/Content/Controls/ColorPicker';
 import Header from './components/Header';
+import { useAppDispatch } from './store/hooks';
+import { changeBackgroundColor } from './store/optionSlice';
+import { getRandomColor } from './utils/colorUtil';
 
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const color = getRandomColor();
+    dispatch(changeBackgroundColor(color));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
 
   return (
     <AppBlock>
+      <ColorPicker
+        whosColorChange='backgroundColor'
+        changeColorFunction={changeBackgroundColor}
+      />
       <Header/>
       <Content/>
     </AppBlock>
