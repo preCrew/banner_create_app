d72b7ac9e52c4b4cc62bf51c8921c5e9dbe47ceb
송민형 <>
2022-07-16 16:03:34 +0900

    add: semicolon in ControlsLayout

--- src/components/Content/Controls/ControlsBox/styles.ts
+++ src/components/Content/Controls/ControlsBox/styles.ts
@@ -4,7 +4,7 @@ export const ControlsLayout = styled.div`
     padding: 16px 0;
     display: flex;
     justify-content: space-between;
-    flex-direction: ${props => props.className ? 'row' && 'column' : 'row'}
+    flex-direction: ${props => props.className ? 'row' && 'column' : 'row'};
 `
 export const ControlsTitle = styled.h3`
     font-size: ${props => props.theme.font.size.fs18}
