b4705fa8b2254076f5358e8100b0590f0ceadc99
송민형 <>
2022-07-16 16:02:47 +0900

    add: padding, justify-content

--- src/components/Content/Controls/ControlsBox/styles.ts
+++ src/components/Content/Controls/ControlsBox/styles.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const ControlsLayout = styled.div`
+    padding: 16px 0;
     display: flex;
+    justify-content: space-between;
     flex-direction: ${props => props.className ? 'row' && 'column' : 'row'}
 `
 export const ControlsTitle = styled.h3`
-    font-size: ${props => props.theme.font.size.fs22}
+    font-size: ${props => props.theme.font.size.fs18}
 `;
