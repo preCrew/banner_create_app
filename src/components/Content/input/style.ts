e8916034b3795803d25bc1119c7ecfc3936fa5a9
송민형 <>
2022-07-19 14:51:21 +0900

    change: input style
    - input style이 받는 prop 정의
    - display 입력 없을시 block

--- src/components/Content/input/style.ts
+++ src/components/Content/input/style.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
-export const InputField = styled.input`
-border-radius: 4px;
-background: #000;
-color: #fff;
-font-size: 18px;
-line-height: 2;
-padding: 2px 2px 2px 8px;
-width: 56px;
-display:block;
+interface TInputField {
+    display?: 'block' |'none';
+}
+export const InputField = styled.input<TInputField>`
+    border-radius: 4px;
+    background: #000;
+    color: #fff;
+    font-size: 18px;
+    line-height: 2;
+    padding: 2px 2px 2px 8px;
+    width: 56px;
+    display:${({display}) => display || 'block'};
 `
-export const InputFieldInline = styled(InputField)`
-display:inline-block;
-`
+// export const InputFieldInline = styled(InputField)`
+//     display:inline-block;
+// `
