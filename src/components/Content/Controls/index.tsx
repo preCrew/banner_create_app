2fae4d413659a681a7ffb877db49adb80cedb57e
이챙 <cycy8527@gmail.com>
2022-07-20 10:10:31 +0900

    Fix: import input 에러

--- src/components/Content/Controls/index.tsx
+++ src/components/Content/Controls/index.tsx
@@ -8,7 +8,7 @@ import {
     changeFontSize, changeForegroundColor, changeHeightRatio ,
     changeTextShadow
 } from '../../../store/optionSlice';
-import Input from '../input';
+import Input from '../Input';
 import {useAppDispatch, useAppSelector} from '../../../store/hooks'
 import { ColorResult } from 'react-color';
 
