9cd7407a05210867d708fc0b2db240459133c884
이챙 <cycy8527@gmail.com>
2022-07-23 05:02:49 +0900

    Style: hedaer layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import HeaderBlock from './style'
+
+import icoGithub from '../../assets/images/ico-github.svg'
 
 interface indexProps {
 
@@ -8,6 +10,10 @@ interface indexProps {
 const Header = () => {
     return (
         <HeaderBlock>
+            <Router>
+                <h1><Link to="/">Banner</Link></h1>
+                <a className="ico-git" target="_blank" href="https://github.com/preCrew/banner_create_app" rel="noreferrer"><img src={icoGithub}/> Gitgub</a>
+            </Router>
         </HeaderBlock>
     );
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,9 +1,26 @@
 import styled from "styled-components";
 
 // TODO: Header 컴포넌트로 교체
-export default styled.div`
-  width:100%;
+export default styled.header`
+  display: flex;
+  justify-content: space-between;
   padding: 16px;
   height: 80px;
-  background: grey;
+
+  & h1{
+    font-size: 1.5rem;
+    font-family: ${props => props.theme.font.style.bold};
+  }
+
+  & .ico-git{
+    font-family: ${props => props.theme.font.style.bold};    
+    img{
+      vertical-align: text-top;
+    }
+  }  
+
+  a {
+    color: rgb(0,0,0);
+    text-decoration:none;
+  }
 `;
