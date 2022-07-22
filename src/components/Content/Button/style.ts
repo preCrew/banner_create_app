35afac4fe1222bd3d1290d61298a84d1122733ef
이챙 <cycy8527@gmail.com>
2022-07-23 04:57:33 +0900

    Rename: 파일->폴더로 이동

--- src/components/Content/Button/style.ts
+++ src/components/Content/Button/style.ts
@@ -34,33 +34,3 @@ export const FgButton = styled(ButtonBlock)`
 export const BdButton = styled(ButtonBlock)`
     background-color: #d69eb3ff;
 `
-
-export const Flex = styled.div`
-display: flex;
-padding: 16px 0;
-align-items: center;
-justify-content: space-between;
-flex-grow: 1;
-`
-
-export const Tabs = styled.div`
-display: flex;
-flex-direction: column;
-border-radius: 4px;
-background-color: #eee;
-flex-grow: 1;
-`
-
-export const Rows = styled.div`
-display: flex;
-`
-
-export const Tab = styled.div`
-display: inline-flex;
-padding: 8px 16px;
-border-radius: 4px;
-cursor: pointer;
-align-items: center;
-justify-content: center;
-flex-grow: 1;
-`
