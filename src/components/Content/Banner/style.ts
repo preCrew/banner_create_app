65afa5a86512be3afd791d5dee5a69a6bde705b3
CSN-ah22 <csa2676@naver.com>
2022-07-31 14:05:45 +0900

    Download&Upload-img newest

--- src/components/Content/Banner/style.ts
+++ src/components/Content/Banner/style.ts
@@ -4,16 +4,17 @@ import { theme } from "../../../styles/theme";
 
 export default styled.div<{
   options: TOptionState;
+
 }>`
   height: 100%;
-  
   outline: 0;
   margin: 16px;
   flex-grow: 1;
   position: sticky;
   top: 16px;
+  
 
-  ${({options}) => css`
+  ${({ options }) => css`
     background: ${options.backgroundColor};
     color: ${options.foregroundColor};
     font-size: ${options.fontSize}px;
@@ -40,4 +41,40 @@ export default styled.div<{
 export const BannerText = styled.div`
   outline: 0;
   text-align: center;
+`;
+
+export const BannerImgBlock = styled.div<{
+  options: TOptionState;
+
+}>`
+  height: 100%;
+  outline: 0;
+  margin: 16px;
+  flex-grow: 1;
+  position: sticky;
+  top: 16px;
+  
+
+  ${({ options }) => css`
+    background-image: url( ${options.backgroundImg});
+    color: ${options.foregroundColor};
+    font-size: ${options.fontSize}px;
+    height: ${options.heightRatio}vw;
+    border ${options.borderWidth}px solid ${options.borderColor};
+    font-weight: ${theme.font.weight.fw700};
+
+    display: flex;
+    /* 가로방향 정렬 */
+    justify-content: ${options.textAlignment.justifyContent};
+    /* 세로방향 정렬 */
+    align-items: ${options.textAlignment.alignItems};
+    /* 텍스트 그림자 */
+    text-shadow: ${options.textShadow.x}px ${options.textShadow.y}px ${options.textShadow.blur}px ${options.textShadow.color};
+
+
+    & ::selection {
+      color: ${options.backgroundColor};
+      background: ${options.foregroundColor};
+    }
+  `}
 `;
